153d6d3b7d71105a003e52402322a25b044068c9
Ktunn <73960645+Ktunn@users.noreply.github.com>
2024-10-23 00:59:53 +0100

    Update score.js

--- js/score.js
+++ js/score.js
@@ -1,15 +1,14 @@
-/
- 
-Numbers of decimal digits to round to*/
+/**
+ * Numbers of decimal digits to round to
+ */
 const scale = 3;
-
-/
- 
-Calculate the score awarded when having a certain percentage on a list level
-@param {Number} rank Position on the list
-@param {Number} percent Percentage of completion
-@param {Number} minPercent Minimum percentage required
-@returns {Number}*/
+/**
+ * Calculate the score awarded when having a certain percentage on a list level
+ * @param {Number} rank Position on the list
+ * @param {Number} percent Percentage of completion
+ * @param {Number} minPercent Minimum percentage required
+ * @returns {Number}
+ */
 export function score(rank, percent, minPercent) {
     if (rank > 150) {
         return 0;  // No points for ranks beyond 150
@@ -17,31 +16,22 @@ export function score(rank, percent, minPercent) {
     if (rank > 75 && percent < 100) {
         return 0;  // Levels above rank 75 only get points if completed 100%
     }
-
     // Cálculo Base da Pontuação
-    let baseScore = (-40 * Math.pow(rank - 1, 0.45) + 350);
-
-
+    let baseScore = (-24.9975 * Math.pow(rank - 1, 0.4) + 200);
     // Fator de completude da percentagem, ajustando para ser mais gradual
     let percentCompletionFactor = (percent - (minPercent - 1)) / (100 - (minPercent - 1));
-
     // Garantir que o fator não é negativo
     percentCompletionFactor = Math.max(0, percentCompletionFactor);
-
     // Pontuação ajustada com o fator de completude
     let score = baseScore * percentCompletionFactor;
-
     // Assegurar que a pontuação é positiva
     score = Math.max(0, score);
-
     // Se a percentagem não for 100%, reduzir a pontuação em um terço
     if (percent !== 100) {
         return round(score - score / 3);
     }
-
     return round(score);
 }
-
 export function round(num) {
     if (!('' + num).includes('e')) {
         return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
